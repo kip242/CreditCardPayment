6484ba56143dca689829800d7e41076b4a2dec93
kip242 <kiptatum@gmail.com>
2019-06-22 10:55:32 -0700

    Add project files.

---
+++ CreditCardPayment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditCardPayment
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double intRate;
+			double Balance;
+			Console.WriteLine("What is the interest rate of the credit card:");
+			intRate = double.Parse(Console.ReadLine());
+			Console.WriteLine("\nWhat is the balance of the credit card:");
+			Balance = double.Parse(Console.ReadLine());
+
+		}
+	}
+}
